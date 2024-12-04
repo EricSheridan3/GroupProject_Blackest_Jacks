800ded8f11f5dc6f1b0a3b52a1147bdb92fd49f0
maurice456 <mauricehenriques@outlook.com>
2024-12-04 15:28:26 -0500

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,26 @@
+public class Card{
+ private int rank;
+ private String type;
+
+  public Card(int rank,String type){
+    this.rank=rank;
+    this.type=type;
+  }
+
+  public int getRank(){
+    return rank;
+  }
+
+  public void setRank(int rank){
+    this.rank=rank;
+  }
+  public String getType(){
+    return type;
+  }
+  public void setType(String type){
+    this.type=type;
+  }
+
+
+  
+}
