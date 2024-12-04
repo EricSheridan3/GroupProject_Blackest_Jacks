76350c600e421a92d17d5e4f472e322ab6c2b39c
maurice456 <mauricehenriques@outlook.com>
2024-12-04 15:32:45 -0500

    Create Deck.java

---
+++ Deck.java
@@ -0,0 +1,9 @@
+public class Deck{
+  private int numberOfCards;
+  public Deck(int numberOfCards){
+    this.numberOfCards=numberOfCards;
+  }
+  public int getNumberOfCards(){
+    return numberOfCards;
+  }
+}
