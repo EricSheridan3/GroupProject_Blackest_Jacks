63df1bf910cd93a06697bc49f689c7c080ef7a03
EricSheridan3 <bertero@sheridancollege.ca>
2024-12-03 19:51:20 -0500

    First Commint

---
+++ Person.java
@@ -0,0 +1,21 @@
+public class Person {
+
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Person(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Person() {
+    }
+
+    
+}
